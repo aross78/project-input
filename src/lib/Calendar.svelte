c1a5c6507f98b13e7e8a864459bdf95d7b11244e
Alyssa Ross <amross@college.harvard.edu>
2024-02-07 14:22:56 -0500

    Fiddling with event handlers in Calendar.svelte

--- src/lib/Calendar.svelte
+++ src/lib/Calendar.svelte
@@ -3,6 +3,12 @@
     import TimeGrid from '@event-calendar/time-grid';
     import Interaction from '@event-calendar/interaction'
 
+    function add_buffer(e, incr){
+
+    }
+
+    
+
     let plugins = [TimeGrid, Interaction];
     let options = {
         view: 'timeGridWeek',
@@ -15,6 +21,11 @@
     function updateOptions() {
         options.slotDuration = '01:00';
     }
+
+    function handleDrag(new_e){
+        options.events = [... options.events, new_e];
+    }
+
 </script>
 
 <Calendar {plugins} {options} />
