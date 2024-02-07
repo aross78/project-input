0a1fdc9d2d792c2da9688f399eb7bf79fc698d78
Alyssa Ross <amross@college.harvard.edu>
2024-02-07 13:59:18 -0500

    Logo

--- src/App.svelte
+++ src/App.svelte
@@ -23,13 +23,6 @@
     <Counter />
   </div>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Hello World!
-  </p>
 </main>
 
 <style>
