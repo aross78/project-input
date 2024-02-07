8c05d58862b372264bdee0978ccc9e7ad586cb04
Alyssa Ross <amross@college.harvard.edu>
2024-02-06 19:08:01 -0500

    Merged

---
+++ src/lib/Calendar.svelte
@@ -0,0 +1,20 @@
+<script>
+    import Calendar from '@event-calendar/core';
+    import TimeGrid from '@event-calendar/time-grid';
+
+    let plugins = [TimeGrid];
+    let options = {
+        view: 'timeGridWeek',
+        selectable: 'true',
+        events: [
+            // your list of events
+        ]
+    };
+
+    function updateOptions() {
+        options.slotDuration = '01:00';
+    }
+</script>
+
+<button on:click={updateOptions}>Change slot duration</button>
+<Calendar {plugins} {options} />
